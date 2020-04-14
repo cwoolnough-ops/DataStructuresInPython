680a0bc89e65b40ee1df5264294e784a912ecfe4
cwoolnough-ops <cwoolnoughh@gmail.com>
2020-04-14 16:22:08 -0600

    heap functions defined

---
+++ heap.py
@@ -0,0 +1,16 @@
+class MinHeap():
+    def __init__(self):
+        self.heap_list = [None]
+
+    def left_child_index(self):
+        pass
+
+    def right_child_index(self):
+        pass
+
+    def heapify_up(self):
+        pass
+    
+    def heapify_down(self):
+        pass
+
